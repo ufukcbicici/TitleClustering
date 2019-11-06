14b8fcc32507e65f453382fd34f45b05537abf8f
ufuk.bicici <ufukcbicici@yahoo.com>
2019-11-06 15:27:29 +0300

    Initial commit

---
+++ corpus.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import re
+
+
+class Corpus:
+    FILTER_REGEX = "[^a-zA-Z\d\s\-]|[\r\n]"
+
+    def __init__(self):
+        self.clearedTitles = None
+
+    def save_cleared_titles(self):
+        with open("cleared_titles.txt", "w") as file:
+            for title in self.clearedTitles:
+                title_unified = ""
+                for word in title:
+                    title_unified += word
+                    title_unified += " "
+                title_unified = title_unified[0:-1]
+                file.write("{0}\n".format(title_unified))
+
+    def load_cleared_titles(self):
+        self.clearedTitles = []
+        with open("cleared_titles.txt", "r") as file:
+            title = file.readline()
+            words = re.split("\s", title)
+            self.clearedTitles.append(np.array(words))
+        self.clearedTitles = np.array(self.clearedTitles)
+
+    def clear_titles(self):
+        titles_df = pd.read_csv('titles.csv')
+        # Clear data
+        self.clearedTitles = []
+        for i in range(titles_df.shape[0]):
+            raw_title = titles_df.loc[i][0]
+            cleared_title = re.sub(Corpus.FILTER_REGEX, "", raw_title)
+            words = re.split("[/\s-]", cleared_title)
+            words = [word.capitalize() for word in words if word != ""]
+            self.clearedTitles.append(np.array(words))
+        self.clearedTitles = np.array(self.clearedTitles)
---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+from corpus import Corpus
+
+
+def test_io():
+    corpus1 = Corpus()
+    corpus1.clear_titles()
+    corpus1.save_cleared_titles()
+
+    corpus2 = Corpus()
+    corpus2.load_cleared_titles()
+
+    assert corpus1.clearedTitles.shape == corpus2.clearedTitles.shape
+    for idx in range(corpus1.clearedTitles.shape[0]):
+        assert np.array_equal(corpus1.clearedTitles[idx], corpus2.clearedTitles[idx])
+
+def main():
+    test_io()
+    print("X")
