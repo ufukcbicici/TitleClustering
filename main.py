3735b087143541a5be97218ec235e68b42f24187
ufukcbicici <ufukcbicici@yahoo.com>
2019-11-08 10:08:25 +0300

    Training word2vec

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import numpy as np
 
 from constants import Constants
 from corpus import Corpus
-from word2vec.cbow_embedding_model import CbowEmbeddingGenerator
+# from word2vec.cbow_embedding_model import CbowEmbeddingGenerator
+import gensim, logging
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
 def test_io():
@@ -18,17 +21,27 @@ def test_io():
         assert np.array_equal(corpus1.clearedTitles[idx], corpus2.clearedTitles[idx])
 
 
+def train_gensim(corpus):
+    sentences = [title.tolist() for title in corpus.clearedTitles if len(title) >= 1]
+    model = gensim.models.Word2Vec(sentences, min_count=1, iter=1000)
+    print("X")
+
+
 def main():
     # test_io()
     corpus = Corpus()
     corpus.clear_titles()
     corpus.build_corpus()
     corpus.read_cbow_data()
-    # corpus.build_contexts()
-    embedding_model = CbowEmbeddingGenerator(corpus=corpus)
-    embedding_model.build_network()
-    embedding_model.test_embedding_network()
-    embedding_model.train()
+    train_gensim(corpus)
+    # sentences = [['first', 'sentence'], ['second', 'sentence']]
+    # # train word2vec on the two sentences
+    # model = gensim.models.Word2Vec(sentences, min_count=1)
+    # # corpus.build_contexts()
+    # # embedding_model = CbowEmbeddingGenerator(corpus=corpus)
+    # # embedding_model.build_network()
+    # # embedding_model.test_embedding_network()
+    # # embedding_model.train()
     print("X")
 
 
