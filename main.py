b8e913b5e251acf760d17550ef72c1a146c9bb7a
ufukcbicici <ufukcbicici@yahoo.com>
2019-11-08 11:58:14 +0300

    Training word2vec

--- main.py
+++ main.py
@@ -23,12 +23,23 @@ def test_io():
 
 def train_gensim(corpus):
     sentences = [title.tolist() for title in corpus.clearedTitles if len(title) >= 1]
+    sentences_without_stop_words = []
+    set_of_stop_words = set()
+    for sentence in sentences:
+        filtered_sentence = []
+        for word in sentence:
+            filtered_word = gensim.parsing.preprocessing.remove_stopwords(word)
+            if filtered_word == word:
+                filtered_sentence.append(filtered_word)
+            else:
+                set_of_stop_words.add(word)
+        sentences_without_stop_words.append(filtered_sentence)
     vocabulary = corpus.vocabulary
-    model = gensim.models.Word2Vec(sentences, min_count=1, iter=100)
+    model = gensim.models.Word2Vec(sentences_without_stop_words, min_count=1, iter=100)
     for word in Constants.VALIDATION_TOKENS:
         similarities = []
         for other_word in vocabulary:
-            if other_word == "UNK":
+            if other_word == "UNK" or other_word in set_of_stop_words:
                 continue
             similarities.append((other_word, model.similarity(word, other_word)))
         similarities = sorted(similarities, key=lambda tpl: tpl[1])
